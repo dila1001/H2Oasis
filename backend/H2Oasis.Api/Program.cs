54e88a9fb0d4849e977af6740bf5420956dbafa0
Adila Razmi <adila93@gmail.com>
2024-01-28 18:30:08 +0100

    feat: set up swagger to run both locally and in prod

--- backend/H2Oasis.Api/Program.cs
+++ backend/H2Oasis.Api/Program.cs
@@ -46,11 +46,9 @@ var configuration = builder.Configuration;
 var app = builder.Build();
 
 {
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.UseHttpsRedirection();
     app.UseCors(policy =>
     {
