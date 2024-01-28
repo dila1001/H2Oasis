6a284bcbb707e48800d31a5e915b1d2d31fe2832
Adila Razmi <adila93@gmail.com>
2024-01-28 18:59:09 +0100

    fix: set production api url

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL =
   process.env.NODE_ENV === 'production'
-    ? 'https://api.example.com'
+    ? 'https://plantapp-salt.azurewebsites.net/api'
     : 'https://localhost:5005/api';
 
 const api = axios.create({
