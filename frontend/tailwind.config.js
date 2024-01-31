60620281d3aa7b7012548d93fcb01e48e58e7951
Silvia Dominguez <silviadmgz@gmail.com>
2024-01-31 14:37:53 +0100

    chore: remove unused module

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,8 +4,7 @@ import daisyui from 'daisyui';
 export default {
 	content: [
 		'./index.html',
-		'./src/**/*.{js,ts,jsx,tsx}',
-		'./node_modules/react-tailwindcss-datepicker/dist/index.esm.js',
+		'./src/**/*.{js,ts,jsx,tsx}'
 	],
 	theme: {
 		extend: {},
