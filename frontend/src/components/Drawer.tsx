646b06f2aed855dd477ca85df15ded5ce4a4d7fd
Adila Razmi <adila93@gmail.com>
2024-02-02 21:57:14 +0100

    refactor: nav drawer

--- frontend/src/components/Drawer.tsx
+++ frontend/src/components/Drawer.tsx
@@ -2,13 +2,16 @@ import { FC, ReactNode } from 'react';
 import { FaSignOutAlt } from 'react-icons/fa';
 import { FaHouse } from 'react-icons/fa6';
 import { baseURL } from '../api/api';
-import AccountInfo from './AccountInfo';
+import { useAuth } from '../auth/useAuth';
+import { generateInitials } from '../utils/account';
 
 type DrawerProps = {
 	children: ReactNode;
 };
 
 const Drawer: FC<DrawerProps> = ({ children }) => {
+	const { user } = useAuth();
+
 	return (
 		<div className='drawer'>
 			<input id='my-drawer' type='checkbox' className='drawer-toggle' />
@@ -20,7 +23,26 @@ const Drawer: FC<DrawerProps> = ({ children }) => {
 					className='drawer-overlay'
 				></label>
 				<ul className='menu p-4 w-80 min-h-full bg-base-200 text-base-content'>
-					<AccountInfo />
+					<div className='flex gap-4 py-5'>
+						{user && (
+							<div className='avatar placeholder'>
+								<div className='bg-neutral text-neutral-content rounded-full w-16'>
+									<span className='text-xl'>{generateInitials(user)}</span>
+								</div>
+							</div>
+						)}
+
+						<div className='flex flex-col justify-center'>
+							{user && (
+								<>
+									<p className='card-title'>
+										{user.firstName} {user.lastName}
+									</p>
+									<p>{user.email}</p>
+								</>
+							)}
+						</div>
+					</div>
 					<div className='border-l h-[1px] bg-base-300 mb-5'></div>
 					<li>
 						<a>
