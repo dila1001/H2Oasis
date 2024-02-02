ccb3e1383d52a780e9646589e8ce2361c97d2fbe
Adila Razmi <adila93@gmail.com>
2024-02-02 01:07:48 +0100

    feat: remove navbar in login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import PlantsPage from './pages/Plants';
@@ -9,21 +9,25 @@ import { AuthGuard, LoginGuard } from './auth/Guards';
 import Login from './pages/Login';
 
 function App() {
-  return (
-    <AuthProvider>
-      <Navbar />
-      <Routes>
-        <Route element={<LoginGuard />}>
-          <Route path='/login' element={<Login />}></Route>
-        </Route>
-        <Route element={<AuthGuard />}>
-          <Route path='/' element={<PlantsPage />}></Route>
-          <Route path='/plant-edit' element={<EditPlant />}></Route>
-          <Route path='/plant/:slug' element={<PlantPage />}></Route>
-        </Route>
-      </Routes>
-    </AuthProvider>
-  );
+	const location = useLocation();
+
+	const isLoginPage = location.pathname === '/login';
+
+	return (
+		<AuthProvider>
+			{!isLoginPage && <Navbar />}
+			<Routes>
+				<Route element={<LoginGuard />}>
+					<Route path='/login' element={<Login />}></Route>
+				</Route>
+				<Route element={<AuthGuard />}>
+					<Route path='/' element={<PlantsPage />}></Route>
+					<Route path='/plant-edit' element={<EditPlant />}></Route>
+					<Route path='/plant/:slug' element={<PlantPage />}></Route>
+				</Route>
+			</Routes>
+		</AuthProvider>
+	);
 }
 
 export default App;
