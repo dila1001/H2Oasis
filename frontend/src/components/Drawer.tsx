fc202cca7d29da46f803e71dbbb4957ff77c7a46
Adila Razmi <adila93@gmail.com>
2024-02-03 10:56:42 +0100

    feat: add link to households when user clicks account info

--- frontend/src/components/Drawer.tsx
+++ frontend/src/components/Drawer.tsx
@@ -37,20 +37,22 @@ const Drawer: FC<DrawerProps> = ({ children }) => {
 					className='drawer-overlay'
 				></label>
 				<ul className='menu p-4 w-80 min-h-full bg-base-200 text-base-content'>
-					<div className='flex gap-4 py-5'>
-						{user && <Avatar user={user} size='xl' />}
+					<Link to='/' onClick={toggleDrawer}>
+						<div className='flex gap-4 py-5'>
+							{user && <Avatar user={user} size='xl' />}
 
-						<div className='flex flex-col justify-center'>
-							{user && (
-								<>
-									<p className='card-title'>
-										{user.firstName} {user.lastName}
-									</p>
-									<p>{user.email}</p>
-								</>
-							)}
+							<div className='flex flex-col justify-center'>
+								{user && (
+									<>
+										<p className='card-title'>
+											{user.firstName} {user.lastName}
+										</p>
+										<p>{user.email}</p>
+									</>
+								)}
+							</div>
 						</div>
-					</div>
+					</Link>
 					<div className='border-l h-[1px] bg-base-300 mb-5'></div>
 					{!households?.length ? (
 						<li>You have no households</li>
