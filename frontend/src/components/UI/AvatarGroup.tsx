fea9b29c68625ee39bcc1613e07afd0c0c08065e
Silvia Dominguez <silviadmgz@gmail.com>
2024-02-03 17:16:46 +0100

    fix: grid layout

--- frontend/src/components/UI/AvatarGroup.tsx
+++ frontend/src/components/UI/AvatarGroup.tsx
@@ -4,11 +4,12 @@ import Avatar from './Avatar';
 
 type AvatarGroupProps = {
 	users: User[];
+	styles?: string;
 };
 
-const AvatarGroup: FC<AvatarGroupProps> = ({ users }) => {
+const AvatarGroup: FC<AvatarGroupProps> = ({ users, styles }) => {
 	return (
-		<div className='avatar-group -space-x-5 rtl:space-x-reverse'>
+		<div className={`avatar-group -space-x-5 rtl:space-x-reverse ${styles}`}>
 			{users.map((user) => (
 				<Avatar key={user.id} user={user} size='xs' />
 			))}
--- frontend/src/pages/Households/HouseholdCard.tsx
+++ frontend/src/pages/Households/HouseholdCard.tsx
@@ -3,6 +3,7 @@ import { User } from '../../services/usersService';
 import { Plant } from '../../services/plantsService';
 import AvatarGroup from '../../components/UI/AvatarGroup';
 import { getDaysLeft } from '../../utils/dateUtils';
+import { FaDroplet, FaLeaf } from 'react-icons/fa6';
 
 type HouseholdCardProps = {
 	householdName: string;
@@ -24,16 +25,21 @@ const HouseholdCard: FC<HouseholdCardProps> = ({
 	});
 
 	return (
-		<div className='card bg-[#6E7E62] shadow-md my-6 flex-row'>
-			<div className='p-4 flex-1 flex-row'>
-				<div>
-					<h2 key={householdName} className='card-title'>
-						{householdName}
-					</h2>
+		<div className='card bg-[#6E7E62] shadow-md my-6 text-white'>
+			<div className='p-4 grid grid-cols-1 gap-2'>
+				<h2 key={householdName} className='card-title justify-self-center'>
+					{householdName}
+				</h2>
 
-					{plants.length > 0 ? (
-						<div className='flex flex-row justify-between'>
+				{plants.length > 0 ? (
+					<div className='flex justify-around col-span-2'>
+						<div className='flex gap-2 items-center'>
+							<FaLeaf />
 							<h2 key={plants.length}>{plants.length} plants</h2>
+						</div>
+
+						<div className='flex gap-2 items-center'>
+							<FaDroplet />
 							{amountToWater.length > 0 && (
 								<h2>{amountToWater.length} need water!</h2>
 							)}
@@ -41,14 +47,21 @@ const HouseholdCard: FC<HouseholdCardProps> = ({
 								<h2>All plants have been watered</h2>
 							)}
 						</div>
-					) : (
-						<div className='flex flex-row justify-between'>
+					</div>
+				) : (
+					<div className='flex justify-around'>
+						<div className='flex gap-2 items-center'>
+							<FaLeaf />
 							<h2>No plants...yet</h2>
+						</div>
+
+						<div className='flex gap-2 items-center'>
+							<FaDroplet />
 							<h2>Add first plant</h2>
 						</div>
-					)}
-					<AvatarGroup users={users} />
-				</div>
+					</div>
+				)}
+				<AvatarGroup users={users} styles='justify-self-center' />
 			</div>
 		</div>
 	);
