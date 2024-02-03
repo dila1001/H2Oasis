a2b44c206fcffb67d82e1aa86f39b87e59923889
Adila Razmi <adila93@gmail.com>
2024-02-03 18:12:30 +0100

    styles: refine styling

--- frontend/src/pages/404/index.tsx
+++ frontend/src/pages/404/index.tsx
@@ -5,9 +5,11 @@ const NotFound = () => {
 	const navigate = useNavigate();
 
 	return (
-		<div className='flex flex-col items-center justify-center h-screen'>
+		<div className='flex flex-col items-center justify-center h-[calc(100vh-64px)] gap-6'>
 			<FaPlantWilt className='text-warning text-[120px]' />
-			<h1>Oops! You seem to be lost.</h1>
+			<h1 className='card-title text-neutral mb-12'>
+				Oops! You seem to be lost.
+			</h1>
 			<button className='btn btn-neutral' onClick={() => navigate(-1)}>
 				Go back
 			</button>
