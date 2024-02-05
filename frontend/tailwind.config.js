694483323a2c74798d04a786729b2489e9401d2b
Adila Razmi <adila93@gmail.com>
2024-02-06 00:17:52 +0100

    fix: dark mode

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,7 +12,7 @@ export default {
 	daisyui: {
 		themes: [
 			{
-				light: {
+				lemonade: {
 					...daisyuiThemes['lemonade'],
 					primary: '#7E9674',
 					error: 'dd7373',
@@ -22,7 +22,7 @@ export default {
 				},
 			},
 		], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: [], // name of one of the included themes for dark mode
+		darkTheme: 'false', // name of one of the included themes for dark mode
 		base: true, // applies background color and foreground color for root element by default
 		styled: true, // include daisyUI colors and design decisions for all components
 		utils: true, // adds responsive and modifier utility classes
