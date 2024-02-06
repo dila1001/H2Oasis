f3ffec81400a4854c9d7bb37ca028d19aef70465
Adila Razmi <adila93@gmail.com>
2024-02-06 08:58:41 +0100

    feat: support for members more than 4 for AvatarGroup

--- frontend/src/components/UI/AvatarGroup.tsx
+++ frontend/src/components/UI/AvatarGroup.tsx
@@ -9,9 +9,16 @@ type AvatarGroupProps = {
 const AvatarGroup: FC<AvatarGroupProps> = ({ users }) => {
 	return (
 		<div className='avatar-group -space-x-5 rtl:space-x-reverse'>
-			{users.map((user) => (
-				<Avatar key={user.id} user={user} size='xs' />
-			))}
+			{users.length < 4 ? (
+				users.map((user) => <Avatar key={user.id} user={user} size='xs' />)
+			) : (
+				<>
+					{[...users.slice(0, 3)].map((user) => (
+						<Avatar key={user.id} user={user} size='xs' />
+					))}
+					<Avatar remainingUsers={users.length - 3} size='xs' />
+				</>
+			)}
 		</div>
 	);
 };
