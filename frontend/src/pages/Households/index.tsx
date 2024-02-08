8bb5ce07f3f0bd73652305010ffde19d28c9c6f5
Silvia Dominguez <silviadmgz@gmail.com>
2024-02-08 13:20:04 +0100

    feat: disabled button when input is empty

--- frontend/src/pages/Households/index.tsx
+++ frontend/src/pages/Households/index.tsx
@@ -27,6 +27,7 @@ const HouseholdsPage = () => {
 		Household | undefined
 	>(undefined);
 	const [isEditingHousehold, setIsEditingHousehold] = useState(false);
+	const [hasValue, setHasValue] = useState('');
 	const [query, setQuery] = useState('');
 	const navigate = useNavigate();
 	const {
@@ -173,6 +174,10 @@ const HouseholdsPage = () => {
 		setQuery(searchQuery);
 	};
 
+	const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setHasValue(event.target.value);
+	};
+
 	return (
 		<div className='mx-5'>
 			<Toaster position='top-center' reverseOrder={false} />
@@ -207,6 +212,7 @@ const HouseholdsPage = () => {
 							className={`input input-bordered input-success w-full ${
 								errors.name && 'input-error'
 							}`}
+							value={hasValue}
 							{...register('name', {
 								required: true,
 								maxLength: {
@@ -214,12 +220,15 @@ const HouseholdsPage = () => {
 									message: 'Name must not exceed 20 characters',
 								},
 							})}
+							onChange={handleOnChange}
 						/>
 						<div className='modal-action'>
 							{errors.name && (
 								<p className='text-error text-sm mt-[-10px] ml-2'>{`${errors.name.message}`}</p>
 							)}
-							<button className='btn' disabled={isSubmitting}>
+							<button className='btn'
+								disabled={!hasValue || isSubmitting}
+							>
 								{isEditingHousehold ? 'Submit' : 'Create'}
 							</button>
 						</div>
