66f7a9847f9ec1e1b85c0e860af634f694944a35
Adila Razmi <adila93@gmail.com>
2024-02-27 11:59:22 +0100

    feat: introduce local db

--- backend/H2Oasis.Api/Program.cs
+++ backend/H2Oasis.Api/Program.cs
@@ -9,8 +9,10 @@ using SameSiteMode = Microsoft.AspNetCore.Http.SameSiteMode;
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
+
 {
-    builder.Services.AddAuthentication(o =>
+    builder.Services
+        .AddAuthentication(o =>
         {
             o.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
         })
@@ -35,22 +37,33 @@ var configuration = builder.Configuration;
             else
             {
                 googleOptions.ClientId = Environment.GetEnvironmentVariable("Google_ClientId");
-                googleOptions.ClientSecret = Environment.GetEnvironmentVariable("Google_ClientSecret");
+                googleOptions.ClientSecret = Environment.GetEnvironmentVariable(
+                    "Google_ClientSecret"
+                );
             }
-            
+
             googleOptions.CallbackPath = "/api/auth/google/callback";
             googleOptions.SaveTokens = true;
         });
-    
+
     builder.Services.AddControllers();
     builder.Services.AddAutoMapper(typeof(Program).Assembly);
     builder.Services.AddScoped<IPlantService, PlantService>();
     builder.Services.AddScoped<IUserService, UserService>();
     builder.Services.AddScoped<IHouseholdService, HouseholdService>();
-    var connection = builder.Environment.IsDevelopment() ? configuration["ConnectionStrings:AZURE_SQL_CONNECTIONSTRING"] : Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING");
-    builder.Services.AddDbContext<PlantDbContext>(options => 
-        options.UseSqlServer(connection, sqlOptionsBuilder => sqlOptionsBuilder.EnableRetryOnFailure()));
-    var blobConnection = builder.Environment.IsDevelopment() ? configuration["Azure:BlobConnString"] : Environment.GetEnvironmentVariable("Azure_Blob_ConnectionString");
+    var connection = builder.Environment.IsDevelopment()
+        ? configuration["ConnectionStrings:AZURE_SQL_Local"]
+        : Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING");
+    builder.Services.AddDbContext<PlantDbContext>(
+        options =>
+            options.UseSqlServer(
+                connection,
+                sqlOptionsBuilder => sqlOptionsBuilder.EnableRetryOnFailure()
+            )
+    );
+    var blobConnection = builder.Environment.IsDevelopment()
+        ? configuration["Azure:BlobConnString"]
+        : Environment.GetEnvironmentVariable("Azure_Blob_ConnectionString");
     builder.Services.AddScoped<BlobServiceClient>(x => new BlobServiceClient(blobConnection));
     builder.Services.AddScoped<IBlobStorageService, BlobStorageService>();
     builder.Services.AddEndpointsApiExplorer();
@@ -59,22 +72,29 @@ var configuration = builder.Configuration;
 
 var app = builder.Build();
 
+
 {
-    app.Use(async (context, next) =>
-    {
-        context.Response.GetTypedHeaders().CacheControl = new CacheControlHeaderValue
+    app.Use(
+        async (context, next) =>
         {
-            NoCache = true
-        };
-        await next();
-    });
+            context.Response.GetTypedHeaders().CacheControl = new CacheControlHeaderValue
+            {
+                NoCache = true
+            };
+            await next();
+        }
+    );
     app.UseSwagger();
     app.UseSwaggerUI();
     app.UseHttpsRedirection();
     app.UseStaticFiles();
     app.UseCors(policy =>
     {
-        policy.WithOrigins("http://localhost:5174", "https://ashy-stone-00b16bb03.4.azurestaticapps.net")
+        policy
+            .WithOrigins(
+                "http://localhost:5174",
+                "https://ashy-stone-00b16bb03.4.azurestaticapps.net"
+            )
             .AllowAnyMethod()
             .AllowAnyHeader()
             .AllowCredentials();
@@ -83,5 +103,3 @@ var app = builder.Build();
     app.MapFallbackToFile("index.html");
     app.Run();
 }
-
-
